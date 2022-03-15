4aabf675b7d7d05e71361e8869e2e8a3d1c921c6
rashad porter <rashad.porter18@gmail.com>
2022-03-15 14:53:48 -0400

    fixed an error with spelling in the serializer and imported model into admin so that i could add to django database

--- products/models.py
+++ products/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
-    price = models.IntegerField()
-    inventory_quantitiy = models.DecimalField(max_digits=8, decimal_places=2)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    inventory_quantity = models.IntegerField()
--- products/views.py
+++ products/views.py
@@ -1,10 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializer import ProductSerializer
+from .models import Product
+from products import serializer
+
+
 
 @api_view(['GET'])
 def products_list(request):
+    products = Product.objects.all()
 
-    return Response('ok')
+    serializer = ProductSerializer(products, many=True)
+
+
+    return Response(serializer.data)
 
 
 
