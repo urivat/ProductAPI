643fd56492911fc1944548369f1f04438d87b170
rashad porter <rashad.porter18@gmail.com>
2022-03-15 12:59:32 -0400

    generated sql workbench tables +updated installed apps with my products file

--- products/models.py
+++ products/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.IntegerField()
+    inventory_quantitiy = models.DecimalField(max_digits=8, decimal_places=2)
