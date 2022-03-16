5396c3e1bd901190a0d7dfd3f29fe6a0104cbc3b
rashad porter <rashad.porter18@gmail.com>
2022-03-16 15:00:42 -0400

    updated apiviews to reflect class based views. updated urls to reflect the change in format.

--- products/urls.py
+++ products/urls.py
@@ -2,6 +2,18 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.products_list),
-    path('<int:pk>/', views.product_details),  
+    
+    path('', views.ProductsList.as_view()),
+    path('<int:pk>/', views.ProductsDetails.as_view()),
 ]
+
+
+
+
+
+
+
+# urlpatterns = [
+#     path('', views.products_list),
+#     path('<int:pk>/', views.product_details),  
+# ]
--- products/views.py
+++ products/views.py
@@ -7,24 +7,27 @@ from rest_framework import status
 from .serializer import ProductSerializer
 from .models import Product
 from products import serializer
-
-class ProductsList(api_view):
+from rest_framework.views import APIView
+class ProductsList(APIView):
 
     def get(self , request, format=None):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
+
     def post(self, request, format):
         serializer = ProductSerializer(data = request.data)
         if serializer.is_valid(raise_exception=True):
            serializer.save()
            return Response(serializer.data, status=status.HTTP_201_CREATED )
         return Response (serializer.errors, status=status.HTTP_404_NOT_FOUND)
-class ProductsDetails(api_view):
+
+
+class ProductsDetails(APIView):
     def get_object(self, pk):
         try:
             return Product.objects.get(pk=pk)
-        except Product.DoesNotExist
+        except Product.DoesNotExist:
             raise Http404 
     #raise is setting an exception within the class if the results are not accepted in a try catch.
     # Can also be used to set a value to a bool if input is not valid. 
@@ -35,11 +38,13 @@ class ProductsDetails(api_view):
 
     def put(self, request, pk, format=None):
         product = self.get_object(pk)
-        serializer= ProductSerializer #product needs to be serialized in order to be translated.
-        if serializer.is_valid(): #have to see if its valid which comes from if you have it or not.
+        serializer= ProductSerializer (product, data=request.data)
+        if serializer.is_valid():     
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+#product needs to be serialized in order to be translated.
+#have to see if its valid which comes from if you have it or not.
     def delete(self, request, pk, format=None):
         product= self.get_object(pk)
         product.delete
