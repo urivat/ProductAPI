ef83875fa1b70b2164d32799778c19b647043268
rashad porter <rashad.porter18@gmail.com>
2022-03-16 11:02:13 -0400

    added image url to the model class and put url on the products.

--- products/models.py
+++ products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image_add = models.CharField(max_length=255)
