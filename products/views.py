7e3407e6019ab1ec64e8f0bc6513a6fc999171ac
rashad porter <rashad.porter18@gmail.com>
2022-03-16 13:42:04 -0400

    added field views into views and created methods to get post put and delete.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,5 @@
+from itertools import product
+from django.http import Http404
 from django.shortcuts import get_list_or_404, get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -6,37 +8,81 @@ from .serializer import ProductSerializer
 from .models import Product
 from products import serializer
 
+class ProductsList(api_view):
 
-
-@api_view(['GET', 'POST'])
-def products_list(request):
-    
-    if request.method == 'GET':
+    def get(self , request, format=None):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self, request, format):
         serializer = ProductSerializer(data = request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid(raise_exception=True):
+           serializer.save()
+           return Response(serializer.data, status=status.HTTP_201_CREATED )
+        return Response (serializer.errors, status=status.HTTP_404_NOT_FOUND)
+class ProductsDetails(api_view):
+    def get_object(self, pk):
+        try:
+            return Product.objects.get(pk=pk)
+        except Product.DoesNotExist
+            raise Http404 
+    #raise is setting an exception within the class if the results are not accepted in a try catch.
+    # Can also be used to set a value to a bool if input is not valid. 
+    def get(self, request, pk , format=None):
+        product = self.get_object(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
 
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_details(request,pk):
-        product = get_object_or_404(Product, pk=pk)
-        if request.method == 'GET':            
-            serializer = ProductSerializer(product)
-            return Response(serializer.data)
-
-        elif request.method == 'PUT':            
-            serializer = ProductSerializer(product, data=request.data)
-            serializer.is_valid(raise_exception=True)
+    def put(self, request, pk, format=None):
+        product = self.get_object(pk)
+        serializer= ProductSerializer #product needs to be serialized in order to be translated.
+        if serializer.is_valid(): #have to see if its valid which comes from if you have it or not.
             serializer.save()
             return Response(serializer.data)
-        elif request.method == 'DELETE':
-            product.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk, format=None):
+        product= self.get_object(pk)
+        product.delete
+        return Response(status=status.HTTP_204_NO_CONTENT)
+ 
+
+
+
+
+
+
+
+
+
+# @api_view(['GET', 'POST'])
+# def products_list(request):
+    
+#     if request.method == 'GET':
+#         products = Product.objects.all()
+#         serializer = ProductSerializer(products, many=True)
+#         return Response(serializer.data)
+#     elif request.method == 'POST':
+#         serializer = ProductSerializer(data = request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# @api_view(['GET', 'PUT', 'DELETE'])
+# def product_details(request,pk):
+#         product = get_object_or_404(Product, pk=pk)
+#         if request.method == 'GET':            
+#             serializer = ProductSerializer(product)
+#             return Response(serializer.data)
+
+#         elif request.method == 'PUT':            
+#             serializer = ProductSerializer(product, data=request.data)
+#             serializer.is_valid(raise_exception=True)
+#             serializer.save()
+#             return Response(serializer.data)
+#         elif request.method == 'DELETE':
+#             product.delete()
+#             return Response(status=status.HTTP_204_NO_CONTENT)
 
     
     
